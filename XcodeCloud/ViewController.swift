21d64dc196bb179c1d82ebc3b8ebc23b3bff2e11
hoang.nk2 <hoang.nk2@seabank.com.vn>
2025-08-08 10:24:05 +0700

    BUild 3

---
+++ XcodeCloud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeCloud
+//
+//  Created by SB14038 on 7/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let hoangnk = "123"
+        print(hoangnk)
+    }
+
+
+}
+
---
+++ XcodeCloudTests/XcodeCloudTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTests.swift
+//  XcodeCloudTests
+//
+//  Created by SB14038 on 7/8/25.
+//
+
+import Testing
+@testable import XcodeCloud
+
+struct XcodeCloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
